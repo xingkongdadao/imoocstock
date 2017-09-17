9095b1c8623699cb9525d552dfefee7832be9c7b
lijiang <lijiang@gogotrans.com>
2017-09-17 23:17:06 +0700

    分割app.component.html到各个组件中

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContentComponent } from './content/content.component';
+import { MenuComponent } from './menu/menu.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { StarsComponent } from './stars/stars.component';
+import { StockManageComponent } from './stock/stock-manage/stock-manage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ContentComponent,
+    MenuComponent,
+    SidebarComponent,
+    StarsComponent,
+    StockManageComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/stock/stock-manage/stock-manage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-stock-manage',
+  templateUrl: './stock-manage.component.html',
+  styleUrls: ['./stock-manage.component.css']
+})
+export class StockManageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
