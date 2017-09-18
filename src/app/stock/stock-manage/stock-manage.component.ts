c8f377591ae666570c798449405864e17adb0c13
lijiang <lijiang@gogotrans.com>
2017-09-18 10:14:25 +0700

    修改stock_manage内容，增加股票数据，调整股票表格

--- src/app/stock/stock-manage/stock-manage.component.ts
+++ src/app/stock/stock-manage/stock-manage.component.ts
@@ -6,10 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./stock-manage.component.css']
 })
 export class StockManageComponent implements OnInit {
+  public stocks: Array<Stock>;
 
   constructor() { }
 
   ngOnInit() {
+    this.stocks = [
+      new Stock(1, "第一支股票",1.99, 3.5, "这是第一只股票，是我在学习慕课网", ["IT", "互联网"]),
+      new Stock(2, "第二支股票",1.99, 3.5, "这是第一只股票，是我在学习慕课网", ["IT", "互联网"]),
+      new Stock(3, "第三支股票",1.99, 3.5, "这是第一只股票，是我在学习慕课网", ["IT", "互联网"]),
+      new Stock(4, "第四支股票",1.99, 3.5, "这是第一只股票，是我在学习慕课网", ["IT", "互联网"])
+
+  ]
   }
 
+
+
 }
+
+export class Stock {
+  constructor(
+    public id: number,
+    public  name: string,
+    public price: number,
+    public rating: number,
+    public desc: string,
+    public categories: Array<string>
+  ){ }
+}
+
