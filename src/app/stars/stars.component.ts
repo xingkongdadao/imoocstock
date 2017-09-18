b79b0abbfdefeeffe74c50da79e192e34b622854
lijiang <lijiang@gogotrans.com>
2017-09-18 14:26:36 +0700

    添加配置星级组件： 星级组件有个评价等级的输入属性。

--- src/app/stars/stars.component.ts
+++ src/app/stars/stars.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-stars',
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./stars.component.css']
 })
 export class StarsComponent implements OnInit {
+  @Input()
+  public rating: number = 0;
+
+  public stars: boolean [];
 
   constructor() { }
 
   ngOnInit() {
+    this.stars = [];
+    for (let i = 1; i <= 5; i++){
+      this.stars.push(i > this.rating);
+    }
+
+    // this.stars = [true,true,true, false,false];
   }
 
 }
