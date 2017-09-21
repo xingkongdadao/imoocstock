9e140b8e96cf9ecb37af2de0fa7c7376fe5e783d
lijiang <lijiang@gogotrans.com>
2017-09-21 13:42:05 +0700

    表单数据绑定

--- src/app/stock/stock-form/stock-form.component.ts
+++ src/app/stock/stock-form/stock-form.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {Stock} from "../../modules/stock";
 import {ActivatedRoute, Router} from "@angular/router";
 import {StockService} from "../../services/stock.service";
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-stock-form',
@@ -9,7 +10,9 @@ import {StockService} from "../../services/stock.service";
   styleUrls: ['./stock-form.component.css']
 })
 export class StockFormComponent implements OnInit {
+  formModel: FormGroup;
   public stock: Stock;
+  public categories = ["IT", "互联网", "金融"];
 
   constructor(
     private routInfo: ActivatedRoute,
@@ -20,6 +23,21 @@ export class StockFormComponent implements OnInit {
     let stockId = this.routInfo.snapshot.params['id'];
     this.stock = this.stockService.getStock(stockId);
 
+    let fb = new FormBuilder();
+    this.formModel = fb.group(
+      {
+        name: [this.stock.name, [Validators.required, Validators.minLength(3)]],
+        price: [this.stock.price, [Validators.required]],
+        desc: [this.stock.desc],
+        categories: fb.array([
+          new FormControl(this.stock.categories.indexOf(this.categories[0]) != -1),
+          new FormControl(this.stock.categories.indexOf(this.categories[1]) != -1),
+          new FormControl(this.stock.categories.indexOf(this.categories[2]) != -1)
+        ])
+
+      }
+    );
+
   }
 
   cancel() {
@@ -27,8 +45,18 @@ export class StockFormComponent implements OnInit {
   }
 
   save() {
-    console.log(this.stock.rating);
-    this.router.navigateByUrl('/stock');
+    let chineseCategories = [];
+    let index = 0;
+    for (let i = 0; i < 3; i++) {
+      if (this.formModel.value.categories[i]) {
+        chineseCategories[index++] = this.categories[i];
+      }
+    }
+
+    this.formModel.value.categories = chineseCategories;
+    this.formModel.value.rating = this.stock.rating;
+    console.log(this.formModel.value);
+    // this.router.navigateByUrl('/stock');
   }
 
 }
