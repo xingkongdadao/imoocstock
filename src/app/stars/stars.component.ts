bbc801b91b4ed27a70d229020d5e25b5947893e0
lijiang <lijiang@gogotrans.com>
2017-09-21 08:11:06 +0700

    控制星级组件可编辑状态

--- src/app/stars/stars.component.ts
+++ src/app/stars/stars.component.ts
@@ -11,6 +11,9 @@ export class StarsComponent implements OnInit {
 
   public stars: boolean [];
 
+  @Input()
+  readonly: boolean = true;
+
   constructor() { }
 
   ngOnInit() {
@@ -20,6 +23,17 @@ export class StarsComponent implements OnInit {
     }
 
     // this.stars = [true,true,true, false,false];
+  }
+
+  clickStar(index: number) {
+    if (!this.readonly) {
+      this.rating = index + 1;
+      this.stars = [];
+      for (let i = 1; i <= 5; i++){
+        this.stars.push(i > this.rating);
+      }
+    }
+
   }
 
 }
